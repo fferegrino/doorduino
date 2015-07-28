7e2383ad0403cb6d03a4f3f2fd302368eb3c5f6b
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-07-27 22:31:37 -0500

    So yeah! is a thing.

---
+++ Watchduino/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+
+namespace Watchduino
+{
+    public class Program
+    {
+
+        static bool on;
+        public static void Main()
+        {
+            var led = new OutputPort(Pins.ONBOARD_LED, false);
+            var inputPort = new InterruptPort(Pins.GPIO_PIN_D7, true, Port.ResistorMode.PullDown, Port.InterruptMode.InterruptEdgeBoth);
+
+            inputPort.OnInterrupt +=inputPort_OnInterrupt;
+            
+
+            while(true)
+            {
+                led.Write(on);
+            }
+
+
+        }
+
+        private static void inputPort_OnInterrupt(uint data1, uint data2, DateTime time)
+        {
+            //Debug.Print("data 1" + data1 + " data2 " + data2);
+            on = data2 == 1;
+        }
+
+    }
+}
