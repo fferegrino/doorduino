7bdfe4c1f5a20bea2db824bbf596a56d822b9471
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-07-31 20:26:01 -0500

    Now is working

--- Watchduino/Program.cs
+++ Watchduino/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.Diagnostics;
 using System.Net.Sockets;
 using System.Threading;
 using Microsoft.SPOT;
@@ -12,27 +13,77 @@ namespace Watchduino
     public class Program
     {
 
-        static bool on;
+        static bool open;
+        static InterruptPort DoorSwitch;
+        static Stopwatch stopWatch;
+        static int seconds;
+        static NewDisplay display;
+        static bool wasOpen;
+
         public static void Main()
         {
-            var led = new OutputPort(Pins.ONBOARD_LED, false);
-            var inputPort = new InterruptPort(Pins.GPIO_PIN_D7, true, Port.ResistorMode.PullDown, Port.InterruptMode.InterruptEdgeBoth);
 
-            inputPort.OnInterrupt +=inputPort_OnInterrupt;
-            
+            var DoorSwitch = new InterruptPort(Pins.GPIO_PIN_D0, true, Port.ResistorMode.PullDown, Port.InterruptMode.InterruptEdgeBoth);
+            stopWatch = Stopwatch.StartNew(); // new Stopwatch();
 
-            while(true)
+            display = new NewDisplay(Pins.GPIO_PIN_D1,
+                                                Pins.GPIO_PIN_D2,
+                                                Pins.GPIO_PIN_D3,
+                Pins.GPIO_PIN_D4, Pins.GPIO_PIN_D5, Pins.GPIO_PIN_D6, Pins.GPIO_PIN_D7, Pins.GPIO_PIN_D8);
+
+
+
+            Thread dsp = new Thread(PrintDisplay);
+            dsp.Start();
+
+
+            wasOpen = DoorSwitch.Read();
+            if (wasOpen)
             {
-                led.Write(on);
+                stopWatch.Start();
             }
+            DoorSwitch.OnInterrupt += DoorSwitch_OnInterrupt;
+            //int  i = 0;
+            //while (i < 1000)
+            //{
+            //    i++;
+            //    seconds++;
+            //    Thread.Sleep(500);
+            //}
 
-
+            Thread.Sleep(Timeout.Infinite);
         }
 
-        private static void inputPort_OnInterrupt(uint data1, uint data2, DateTime time)
+
+        public static void PrintDisplay()
         {
-            //Debug.Print("data 1" + data1 + " data2 " + data2);
-            on = data2 == 1;
+            while (true)
+            {
+                display.PrintNumber(seconds);
+            }
+        }
+
+        private static void DoorSwitch_OnInterrupt(uint data1, uint data2, DateTime time)
+        {
+            open = data2 == 1;
+            if (!open)
+            {
+                Debug.Print("Opened");
+                seconds = 0;
+                stopWatch.Start();
+            }
+            else
+            {
+                Debug.Print("Closed");
+                if (!wasOpen)
+                {
+                    stopWatch.Stop();
+                }
+                seconds = (int)stopWatch.ElapsedMilliseconds;
+                Debug.Print("Tiempo abierto: " + seconds);
+                stopWatch.Reset();
+                wasOpen = false;
+            }
         }
 
     }
