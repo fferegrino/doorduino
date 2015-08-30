508db81a1e5f39b43d4ae7bafb71a3e0eaa48507
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-08-30 15:07:35 -0500

    Huge commit. Naming.

---
+++ Doorduino.MobileApp/Doorduino/App.cs
@@ -0,0 +1,59 @@
+﻿using Messier16.Forms.Plugin.Xparse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Doorduino
+{
+    public class App : Application
+    {
+        public App()
+        {
+            var btn = new Button() { Text = "Suscribir" };
+            var lbl = new Label
+            {
+                XAlign = TextAlignment.Center,
+                Text = "Welcome to Xamarin Forms!"
+            };
+            btn.Clicked += async (sender, args) =>
+            {
+                await CrossParse.SuscribeAsync();
+                lbl.Text = "Listo";
+                CrossParse.AddParsePushNotificationReceivedListener();
+                CrossParse.OnPushReceived += (s, a) =>
+                {
+                    lbl.Text = a.Payload["alert"].ToString();
+                };
+            };
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        lbl,
+                        btn
+                    }
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            //CrossParse.InitializeClient(ParseKeys.AppId, ParseKeys.NetKey);
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Doorduino.MobileApp/Doorduino/ParseKeys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doorduino
+{
+    public class ParseKeys
+    {
+
+        public const string NetKey = "[Net Key]";
+
+        public const string AppId = "[App ID]";
+    }
+}
